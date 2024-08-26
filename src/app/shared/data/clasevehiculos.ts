420a7c32b3e3bf28517eba8ad97987a1a0e01b37
Cristian Algecira <cristian.algecira.99@gamil.com>
2024-08-26 09:41:11 -0500

    Cambio 7 en vista incremento

---
+++ src/app/shared/data/clasevehiculos.ts
@@ -0,0 +1,9 @@
+export const CLASEVEHICULOS = [
+  { value: 'Bus', label: 'Bus' },
+  { value: 'Microbus', label: 'Microbus' },
+  { value: 'Buseta', label: 'Buseta' },
+  { value: 'Camioneta DC', label: 'Camioneta DC' },
+  { value: 'Camioneta SW', label: 'Camioneta SW' },
+  { value: 'Automóvil', label: 'Automóvil' },
+  { value: 'Camioneta de platón', label: 'Camioneta de platón' },
+];
