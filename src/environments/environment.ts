9b1a00c53de936de2e0e075b34c3651e0bb1e162
Juan Pablo Suarez Ramirez <jpsuarez40@gmail.com>
2024-10-04 14:49:27 -0500

    actu fijacion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     API_URL: 'http://localhost:9090',
-    MAESTROS: 'http://48.216.166.9/transversales/datos-maestros/',
+    MAESTROS: 'http://48.216.166.9/transversales/datos-maestros',
     RUES: 'http://104.211.39.160:8000',
 };
