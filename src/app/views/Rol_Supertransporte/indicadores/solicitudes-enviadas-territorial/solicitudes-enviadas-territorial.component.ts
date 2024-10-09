a3f4345130f6ecdf2c6227db8b36ab8426f5aa53
Juan Pablo Suarez Ramirez <jpsuarez40@gmail.com>
2024-10-09 09:21:56 -0500

    actu indicadores

---
+++ src/app/views/Rol_Supertransporte/indicadores/solicitudes-enviadas-territorial/solicitudes-enviadas-territorial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitudes-enviadas-territorial',
+  standalone: true,
+  imports: [],
+  templateUrl: './solicitudes-enviadas-territorial.component.html',
+  styleUrl: './solicitudes-enviadas-territorial.component.css'
+})
+export default class SolicitudesEnviadasTerritorialComponent {
+
+}
